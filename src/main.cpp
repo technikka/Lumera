58e20a20432d8cb565951e490b45b7b3bc3f80f7
Nikki Brusoe <nikkibrusoe@gmail.com>
2026-08-05 10:28:37 -0700

    Initial project setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,34 @@
 #include <SFML/Graphics.hpp>
+#include <optional>
 
 int main()
 {
-	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
-	sf::CircleShape shape( 100.f );
-	shape.setFillColor( sf::Color::Green );
+    sf::RenderWindow window(sf::VideoMode({800, 800}), "Lumera");
+    sf::CircleShape circle(20.f);
+    circle.setFillColor(sf::Color::Blue);
 
-	while ( window.isOpen() )
-	{
-		while ( const std::optional event = window.pollEvent() )
-		{
-			if ( event->is<sf::Event::Closed>() )
-				window.close();
-		}
+    auto size = window.getSize();
 
-		window.clear();
-		window.draw( shape );
-		window.display();
-	}
+    // Set the origin to the center of the circle.
+    auto bounds = circle.getLocalBounds();
+    circle.setOrigin({bounds.position.x + bounds.size.x / 2.f,
+                      bounds.position.y + bounds.size.y / 2.f});
+
+    // Center it in the window.
+    auto windowSize = window.getSize();
+    circle.setPosition({windowSize.x / 2.f,
+                        windowSize.y / 2.f});
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear(sf::Color::White);
+        window.draw(circle);
+        window.display();
+    }
 }
